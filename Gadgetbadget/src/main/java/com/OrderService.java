8f6ebdda621b448c7d536ceb63309c2fbbfed243
amasheshadee <53006150+amasheshadee@users.noreply.github.com>
2021-04-22 15:40:23 +0530

    Add ordering CRUD operations

---
+++ Gadgetbadget/src/main/java/com/OrderService.java
@@ -0,0 +1,65 @@
+package com;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/payment")
+public class OrderService {
+	
+
+	OrderService orderService = new OrderService();
+
+	
+
+	// Create transaction
+	@POST
+	@Path("/create")
+	@Produces(MediaType.TEXT_HTML)
+	public String createOrders(
+
+			@FormParam("orderID") int orderID, @FormParam("nuOfItems") int nuOfItems,
+			@FormParam("BuyerID") String BuyerID, @FormParam("amount") double amount,
+			@FormParam("amount") String orderDate)
+
+	{
+
+		return orderService.createOrders(orderID, nuOfItems, BuyerID, amount,orderDate);
+	}
+
+	
+	
+	
+	// Find order by order ID
+	@GET
+	@Path("/id/{orderID}")
+	public String getOrderById(@FormParam("orderID") String orderID) {
+
+		return orderService.getOrderById(orderID);
+	}
+	
+	
+	
+
+	// Delete an order
+	@DELETE
+	@Path("/delete/{orderID}")
+	public String deleteOrder(@FormParam("orderID") String orderID) {
+
+		return orderService.deleteOrder(orderID);
+	}
+
+	// Update an order
+	@PUT
+	@Path("/")
+	public String updateOrder(@FormParam("orderID") String orderID) {
+
+		return orderService.updateOrder(orderID);
+	}
+}
+		
---
+++ Gadgetbadget/src/main/java/model/Order.java
@@ -0,0 +1,186 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Order {
+
+	// Database Connection
+	private Connection connect() {
+		Connection con = null;
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Access Database 'Gadgetbadget'
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gadgetbadget", "root", "");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	// Create Order
+	public String creatOrder(int orderID, String BuyerID, int nuOfItems, double amount, String orderDate) {
+
+		String printMsg = "";
+
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Database Connecting Error - Post";
+			}
+
+			// create a Prepared Statement
+			String DBquery = " insert into order(`orderID` , `BuyerID`, `nuOfItems`, `amount`, `orderDate`)"
+					+ " values (?, ?, ?, ?, ?)";
+
+			PreparedStatement preparedStmt = con.prepareStatement(DBquery);
+
+			// Set Values
+			preparedStmt.setInt(1, orderID);
+			preparedStmt.setInt(2, Integer.parseInt(BuyerID));
+			preparedStmt.setInt(3, nuOfItems);
+			preparedStmt.setDouble(4, amount);
+			preparedStmt.setString(5, orderDate);
+
+			// Execute the Prepared Statement
+			preparedStmt.execute();
+			con.close();
+
+			// Print Success Message
+			printMsg = "Inserted Successful";
+
+		} catch (Exception e) {
+
+			// Print Failed Message
+			printMsg = "ERROR! Insertion Failed";
+
+			System.err.println(e.getMessage());
+		}
+
+		return printMsg;
+
+	}
+	
+	//Update an Existing Order
+		public String updateOrder(int orderID, String BuyerID, int nuOfItems, double amount, String orderDate)
+		{
+			 String printMsg = "";
+			 try
+			 {
+			 Connection con = connect();
+			 if (con == null)
+			 {return "Error while connecting to the database for updating."; }
+			 
+			 
+			 // create a prepared statement
+			 String DBquery = "UPDATE orders SET orderID=?,BuyerID=?,nuOfItems=?,amount=?,orderDate=?";
+			 PreparedStatement preparedStmt = con.prepareStatement(DBquery);
+			 
+			 // binding values
+			 	preparedStmt.setInt(1, orderID);
+				preparedStmt.setInt(2, Integer.parseInt(BuyerID));
+				preparedStmt.setInt(3, nuOfItems);
+				preparedStmt.setDouble(4, amount);
+				preparedStmt.setString(5, orderDate);
+				
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 printMsg = "Updated order successfully";
+			 }
+			 
+			 catch (Exception e)
+			 {
+				 printMsg = "Error while updating the order.";
+				 System.err.println(e.getMessage());
+			 }
+			 
+			 return printMsg;
+			}
+		
+		
+		
+		
+	// View order by orderID
+		public String getOrderById(String orderID) {
+
+			String printMsg = "";
+
+			try {
+				Connection con = connect();
+
+				if (con == null) {
+					return "ERROR while connecting to the DATABASE";
+				}
+
+				// create a prepared statement
+				String DBquery = "SELECT * FROM Orde WHERE orderID =" + " '" + orderID + "' ";
+
+				PreparedStatement pst = con.prepareStatement(DBquery);
+				ResultSet rst = pst.executeQuery(DBquery);
+
+				while (rst.next()) {
+
+					String OrderMethod = rst.getString("ORDER_METHOD");
+					String BuyerID= rst.getString("BUYER_ID");
+					String TotalAmount = rst.getString("TOTAL_AMOUNT");
+
+					printMsg = "OrderMethod : " + OrderMethod + " & " + "BuyerID : " + BuyerID + " & " + "Total Amount : "
+							+ TotalAmount;
+
+				}
+
+				// execute the statement
+				con.close();
+			}
+
+			catch (Exception e) {
+				printMsg = "Error while Finding Order.";
+				System.err.println(e.getMessage());
+			}
+
+			return printMsg;
+		}
+
+	// Delete an Existing Order
+	public String deletOrder(int orderID) {
+		
+		String printMsg = "";
+		
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Database Connecting Error - Delete";
+			}
+			
+			// create a prepared statement
+			String DBquery = "delete from orders where orderID=?";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(DBquery);
+			
+			// binding values
+			preparedStmt.setInt(1, orderID);
+			
+			// execute the statement
+			preparedStmt.execute();
+			
+			con.close();
+			
+			printMsg = "Order Deleted successfully";
+			
+		} catch (Exception e) {
+			
+			printMsg = "Error while deleting the item.";
+			System.err.println(e.getMessage());
+		}
+		
+		return printMsg;
+	}
+	
+
+}
